94524cf78da7150696fe66aebf632869b8b822f4
David Data Engineer <128224812+davedataengineer@users.noreply.github.com>
2023-03-22 15:58:35 +0000

    Update average_trip_distance.sql

--- models/average_trip_distance.sql
+++ models/average_trip_distance.sql
@@ -1,12 +1,8 @@
 {{ config(materialized='table') }}
 
 with source_data as (
-
-    select null as average_trip_distance from default.trips
-    -- select avg( trip_distance ) as average_trip_distance from default.trips
-
+    select distinct null as average_trip_distance from default.trips
 )
 
 select *
 from source_data
--- where id is not null
